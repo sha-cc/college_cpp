b349795e18d1d38a2fdbb3a0c3be50863f2fd001
shacc <ilyasteterinus@yandex.ru>
2022-02-08 22:15:55 +0300

    Документация к seven/1

--- seven/1.cpp
+++ seven/1.cpp
@@ -4,35 +4,36 @@ using namespace std;
 
 int main(void) {
 
-    const int SIZE = 10;            // размер массива - константа
+    const int SIZE = 10;                // размер массива - константа
 
-    float arr[SIZE];
+    float arr[SIZE];                    // создаем массив
 
     while (1) {
         char input;
         cout << "\n\033[94mИнициализация\033[0m: случайная\033[92m(r)\033[0m или вручную\033[92m(m)\033[0m? ";
-        input = getchar();
-        for (int i = 0; i < 45; i++) {
-            printf("-");
-        }
-        printf("\n");
+        input = getchar();              // использовать getchar не обязательно, можно обойтись cin
+        
+        for (int i = 0; i < 45; i++) {  // эта непонятная конструкция просто выводит
+            printf("-");                // линию из черточек на экран. От этого куска
+        }                               // кода можно с легкостью избавиться, он тут
+        printf("\n");                   // (почти) не нужен, только для визуала.
 
-        if (input == 'm') {
+        if (input == 'm') {             // m - manual
             for (int i = 0; i < SIZE; i++) {
                 printf("\033[0mВведите \033[95m%d\033[0m элемент массива: \033[91m", i);
-                scanf("%f", &arr[i]);
+                scanf("%f", &arr[i]);   // scanf тоже можно заменить на cin
                 if (i == SIZE)
-                    goto brk;       // break
+                    goto brk;           // break
             }
-        } else if (input == 'r') {
+        } else if (input == 'r') {      // r - random
             for (int i = 0; i < SIZE; i++) {
-                arr[i] = (float(rand())/float(RAND_MAX)) * 6.9; // магия
+                arr[i] = (float(rand())/float(RAND_MAX)) * 6.9; // я не знаю, как это работает. Но работает же!
                 if (i == SIZE)
-                    goto brk;       // break
+                    goto brk;           // break
             }
-        } else { continue; }
+        } else { continue; }            // смысл цикла был только в том, что плохой ввод можно вот так игнорировать
 
-        brk: break;
+        brk: break;                     // и да, я знаю, что goto не надо использовать. Cry about it lol
     }
 
     // вывод содержимого
