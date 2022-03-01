6c894dbc592cf849bedb8eb17cd36fb01f5b6ba8
shacc <ilyasteterinus@yandex.ru>
2022-03-01 08:27:19 +0300

    not finished

--- ten/2_var8.cpp
+++ ten/2_var8.cpp
@@ -39,7 +39,72 @@ int main(void) {
     printf("\033[0m\n");
 
 
+    // https://www.geeksforgeeks.org/print-given-matrix-counter-clock-wise-spiral-form/
 
+    int k = 5, l = 0;
+    int m = arrX, n = arrY;
+ 
+    //  k - starting row index
+    //    m - ending row index
+    //    l - starting column index
+    //    n - ending column index
+ 
+    // initialize the count
+    int cnt = 0;
+ 
+    // total number of
+    // elements in matrix
+    int total = arrX * arrY;
+
+    while (l < n) {
+        if (cnt == total)
+            break;
+ 
+        // Print the first column
+        // from the remaining columns
+        for (int i = k; i < m; ++i) {
+            cout << arr[i][l] << " ";
+            cnt++;
+        }
+        l++;
+ 
+        if (cnt == total)
+            break;
+ 
+        // Print the last row from
+        // the remaining rows
+        for (int i = l; i < n; ++i) {
+            cout << arr[m - 1][i] << " ";
+            cnt++;
+        }
+        m--;
+ 
+        if (cnt == total)
+            break;
+ 
+        // Print the last column
+        // from the remaining columns
+        if (k < m) {
+            for (int i = m - 1; i >= k; --i) {
+                cout << arr[i][n - 1] << " ";
+                cnt++;
+            }
+            n--;
+        }
+ 
+        if (cnt == total)
+            break;
+
+        // Print the first row
+        // from the remaining rows
+        if (l < n) {
+            for (int i = n - 1; i >= l; --i) {
+                cout << arr[k][i] << " ";
+                cnt++;
+            }
+            k++;
+        }
+    }
 
     printf("\n");
 
