071e2f8f9fee9b33d5aa3f239b4f5f9e5954adbd
shacc <79783139+sha-cc@users.noreply.github.com>
2022-04-04 04:21:38 +0300

    Учёт полностью положительных строк

--- twelve/all_var11.cpp
+++ twelve/all_var11.cpp
@@ -37,7 +37,7 @@ int main(void) {
     int *arrOfNegatives = (int*) (malloc(m * sizeof(int)));
 
     for (int i = 0; i < m; i++)
-        arrOfNegatives[i] = 0;          // заполнение массива нулями
+        arrOfNegatives[i] = -1;          // заполнение массива (если в строке нет отрицательных элементов, i = -1)
 
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++) {
